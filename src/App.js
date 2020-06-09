d75e3ec2fddd71ecc9c36ef12ee2798b4e09077b
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-09 18:39:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Github Actions.
         </a>
       </header>
     </div>
